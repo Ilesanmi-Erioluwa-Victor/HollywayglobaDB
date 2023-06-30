8611024b1f343e6209557c8a6c52a9d12e3b9e37
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-01 00:25:26 +0100

    Working on types

--- src/types/requestErrorType.ts
+++ src/types/requestErrorType.ts
@@ -1,3 +1,10 @@
 interface DocumentResult<T> {
     _doc : T
 }
+
+export interface cartItemTypes extends DocumentResult<cartItemTypes> {
+    productId: object;
+    userId : object;
+    productCount: number;
+    totalAmount : number
+}
