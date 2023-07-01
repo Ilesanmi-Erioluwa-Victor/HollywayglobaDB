be034aa0b88c0d37291e515ffa7d67702bd2913c
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-07-01 21:50:52 +0100

    working on admin Model

--- src/model/admin/model.admin.ts
+++ src/model/admin/model.admin.ts
@@ -1,29 +1,41 @@
-import mongoose, { Document, Model, Schema } from 'mongoose';
-
+import mongoose, { Document, Model, Schema } from "mongoose";
 
 interface adminModel extends Document {
-    email: string;
-    username: string;
-    password: string;
+  email: string;
+  username: string;
+  password: string;
 }
 
-const adminSchema = new Schema<adminModel>({
+interface adminModelemailTaken extends Model<adminModel> {
+  emailTaken: (email: string) => Promise<boolean>;
+}
+
+const adminSchema = new Schema<adminModel, adminModelemailTaken>(
+  {
     email: {
-        type: String,
-        required : true
+      type: String,
+      required: true,
     },
 
     username: {
-        type : String,
-        required : true
+      type: String,
+      required: true,
     },
 
-    password : {
-        type : String,
-        required : true
-    }
-}, {
-    timestamps : true
-})
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
 
-export const AdminModel = mongoose.model<adminModel>('adminModel', adminSchema)
+export const AdminModel = mongoose.model<adminModel>("adminModel", adminSchema);
