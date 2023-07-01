bfb8473f3049e30812b286a42969e472ea772435
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-07-01 21:27:37 +0100

    working on admin controller

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
-module.exports = (fn: any) => {
-  return (req:Request, res:Response, next: NextFunction) => {
-    fn(req, res, next).catch((err:any) => next(err));
+export const catchAsync= (fn: any) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch((err: any) => next(err));
   };
 };
