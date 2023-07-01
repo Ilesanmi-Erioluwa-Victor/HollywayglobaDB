3ba90d42d8aa1e7be3576c1e84ad320860923816
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-01 20:13:36 +0100

    Working on Model

--- dist/model/admin/model.admin.js
+++ dist/model/admin/model.admin.js
@@ -1,2 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const adminSchema = new mongoose_1.Schema({
+    email: {
+        type: String,
+    }
+});
--- src/model/admin/model.admin.ts
+++ src/model/admin/model.admin.ts
@@ -3,4 +3,12 @@ import mongoose, { Document, Model, Schema } from 'mongoose';
 
 interface adminModel extends Document {
     email: string;
+    username: string;
+    password: string;
 }
+
+const adminSchema = new Schema<adminModel>({
+    email: {
+        type : String,
+    }
+})
