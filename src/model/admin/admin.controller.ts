1d63ebe60b6972c5b5875a9a3dde9311f9a0d7d3
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-07-01 21:27:37 +0100

    working on admin controller

--- src/model/admin/admin.controller.ts
+++ src/model/admin/admin.controller.ts
@@ -4,6 +4,17 @@ import { StatusCodes } from "http-status-codes";
 import { Jwt } from "jsonwebtoken";
 import { catchAsync } from '../../utils/catchAsync'
 
-export const signUp : RequestHandler = catchAsync(async (params:type) => {
-    
+export const signUp : RequestHandler = catchAsync(async (req, res, next) => {
+
+    try {
+          const admin = AdminModel.create({
+            name: req.body.name,
+            password: req.body.password,
+            username: req.body.username,
+          });
+          
+    } catch (error) {
+        
+    }
+  
 })
