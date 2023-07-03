4823f09c5f22ac9c0930cab3126313094fc685f7
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-03 20:24:01 +0100

    Working on user controller

--- src/model/user/user.controller.ts
+++ src/model/user/user.controller.ts
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcryptjs';
-import { RequestHandler } from 'express';
+import { RequestHandler, NextFunction, Request, RequestParamHandler } from 'express';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { throwError } from "../../middlewares/cacheError";
 import { StatusCodes } from 'http-status-codes';
 
 import { userModel } from "./model.user";
+import { catchAsync } from "../../utils/catchAsync";
 
 dotenv.config();
+
+export const create_user: RequestHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const user = await userModel.create({
+        first_name: req.body.first_name,
+        last_name : req.body.last_name
+    })
+})
