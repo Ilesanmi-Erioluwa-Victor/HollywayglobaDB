cdae28d906a68ed2a1c82f54ede1b1cb711f6f77
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-03 20:41:26 +0100

    Working on user controller

--- src/model/user/user.controller.ts
+++ src/model/user/user.controller.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcryptjs';
-import { RequestHandler, NextFunction, Request, RequestParamHandler } from 'express';
+import { RequestHandler, NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { throwError } from "../../middlewares/cacheError";
@@ -13,13 +13,22 @@ dotenv.config();
 
 export const create_user: RequestHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { first_name, last_name, password, email } = req.body;
+    const exist_user = await userModel.findOne({ email });
+    const hashedPassword = await bcrypt.hash(password, 12);
+    
+    if (exist_user) throwError("You are already a member, kindly login to your account", StatusCodes.CONFLICT);
     const user = await userModel.create({
         first_name,
         last_name,
-        password,
-        email
+        email,
+        password : hashedPassword,
     })
 
-    const exist_user = await userModel.findOne({ email })
-    if (exist_user) throwError("You are already a member, kindly login to your account", StatusCodes.CONFLICT);
+    await user.save();
+    res.status(StatusCodes.CREATED).json({
+        message: "You have successfully created your account, log in now",
+        status: "success",
+        userId : user?._id
+    })
+
 })
