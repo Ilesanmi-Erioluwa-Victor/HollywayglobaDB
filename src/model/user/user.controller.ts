41553a8a9848322da5e137dde7d8162429839a0a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-03 20:10:16 +0100

    Working on user model

--- src/model/user/user.controller.ts
+++ src/model/user/user.controller.ts
@@ -1,3 +1,9 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
+import { RequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { throwError } from "../../middlewares/cacheError";
+import { StatusCodes } from 'http-status-codes';
 
 
