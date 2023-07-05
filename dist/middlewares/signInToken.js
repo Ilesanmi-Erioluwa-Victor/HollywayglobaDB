7c220051b6f8bad4461061e4a3a87a870d0d0bc3
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-05 16:37:38 +0100

    Working on resetpassword route

--- dist/middlewares/signInToken.js
+++ dist/middlewares/signInToken.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const signToken = (id) => {
-    return jsonwebtoken_1.default.sign({ id }, process.env.JWT_SERCRET_KEY, {
+    return jsonwebtoken_1.default.sign({ id }, `${process.env.JWT_SERCRET_KEY}`, {
         expiresIn: process.env.JWT_EXPIRES_IN,
     });
 };
--- src/middlewares/signInToken.ts
+++ src/middlewares/signInToken.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
-const signToken = (id: any) => {
-  return jwt.sign({ id }, process.env.JWT_SERCRET_KEY, {
+const signToken = (id) => {
+  return jwt.sign({ id }, `${process.env.JWT_SERCRET_KEY}`, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
