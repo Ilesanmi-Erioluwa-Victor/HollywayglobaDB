2e1d38d85b21bbf61bb6a9346476422ea308d099
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-05 16:41:59 +0100

    Working on resetpassword route

--- src/middlewares/signInToken.ts
+++ src/middlewares/signInToken.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
 
-const signToken = (id) => {
+const signToken = (id:any) : string => {
   return jwt.sign({ id }, `${process.env.JWT_SERCRET_KEY}`, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
-module.exports = signToken;
+export default  signToken;
