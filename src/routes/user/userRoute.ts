f4cdfe58e73b2c4b8dafa568da873569f988b5d5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-07 19:01:13 +0100

    Working on userController

--- src/routes/user/userRoute.ts
+++ src/routes/user/userRoute.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { create_user, login_user, forgot_password,reset_password, get_users } from "../../model/user/user.controller";
+import { create_user, login_user, forgot_password,reset_password, get_users, delete_user } from "../../model/user/user.controller";
 
 const userRouter = express.Router();
 
@@ -7,6 +7,7 @@ userRouter.post("/signup", create_user);
 userRouter.post("/login", login_user)
 
 userRouter.get('/', get_users);
+userRouter.delete('/:id', delete_user);
 userRouter.post("/forget_password",forgot_password);
 userRouter.patch("/reset_password/:token",reset_password);
 
