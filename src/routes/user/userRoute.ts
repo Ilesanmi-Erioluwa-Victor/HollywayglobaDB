580cd48ff21914444efb12aef22c424c16d87fb5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-07 18:51:41 +0100

    Working on get users

--- src/routes/user/userRoute.ts
+++ src/routes/user/userRoute.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { create_user, login_user, forgot_password,reset_password } from "../../model/user/user.controller";
+import { create_user, login_user, forgot_password,reset_password, get_users } from "../../model/user/user.controller";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", create_user);
 userRouter.post("/login", login_user)
 
+userRouter.get('/', get_users);
 userRouter.post("/forget_password",forgot_password);
 userRouter.patch("/reset_password/:token",reset_password);
 
