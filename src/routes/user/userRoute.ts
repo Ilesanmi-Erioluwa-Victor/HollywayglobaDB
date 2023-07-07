bc736505fdd6990d480e8ba0fcf7a2c729018f35
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-07 20:19:23 +0100

    creating controller for users

--- src/routes/user/userRoute.ts
+++ src/routes/user/userRoute.ts
@@ -1,14 +1,23 @@
-import express from "express";
-import { create_user, login_user, forgot_password,reset_password, get_users, delete_user } from "../../model/user/user.controller";
+import express from 'express';
+import {
+  create_user,
+  login_user,
+  forgot_password,
+  reset_password,
+  get_users,
+  get_user,
+  delete_user,
+} from '../../model/user/user.controller';
 
 const userRouter = express.Router();
 
-userRouter.post("/signup", create_user);
-userRouter.post("/login", login_user)
+userRouter.post('/signup', create_user);
+userRouter.post('/login', login_user);
 
 userRouter.get('/', get_users);
+userRouter.get('/:id', get_user);
 userRouter.delete('/:id', delete_user);
-userRouter.post("/forget_password",forgot_password);
-userRouter.patch("/reset_password/:token",reset_password);
+userRouter.post('/forget_password', forgot_password);
+userRouter.patch('/reset_password/:token', reset_password);
 
 export default userRouter;
