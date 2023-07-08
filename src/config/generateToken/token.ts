3b729b9340b0fb4f5dd88befad510d084f4348bf
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-08 19:28:05 +0100

    fixed a bug in generateToken/token.ts

--- src/config/generateToken/token.ts
+++ src/config/generateToken/token.ts
@@ -6,7 +6,7 @@ import { StatusCodes } from 'http-status-codes';
 dotenv.config();
 
 const generateToken = (id: string) => {
-  if (!process.env.JWT_KEY)
+  if (!process.env.JWT_SERCRET_KEY)
     throwError('JWT_KEY is required in environment', StatusCodes.BAD_REQUEST);
   return jwt.sign({ id }, `${process.env.JWT_SERCRET_KEY}`, {
     expiresIn: `${process.env.JWT_EXPIRES_IN}`,
