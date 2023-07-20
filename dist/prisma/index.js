90409b79e724873b939d7871d33ea7607e1b290a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-20 21:58:18 +0100

    All logic working perfectly well in prisma

--- dist/prisma/index.js
+++ dist/prisma/index.js
@@ -1,7 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.prisma = void 0;
 const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient({
-    log: ['info', 'error'],
-});
-exports.default = prisma;
+exports.prisma = new client_1.PrismaClient();
