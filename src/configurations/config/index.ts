d1221a15f3ec436bf329271e07f7a633e9e1dc72
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-25 16:47:44 +0100

    Modifying my apps

--- src/configurations/config/index.ts
+++ src/configurations/config/index.ts
@@ -1,5 +1,5 @@
-const JWT_SERCRET_KEY = process.env.JWT_SERCRET_KEY;
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;   
+const JWT_SECRET_KEY = process.env.JWT_SERCRET_KEY;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 const PORT = process.env.PORT;
 const CRYPTO = process.env.CRYPTO;
 const NODEMAILER_PASS = process.env.NODEMAILER_PASS;
@@ -7,5 +7,16 @@ const NODEMAILER_USERNAME = process.env.NODEMAILER_USERNAME;
 const DATABASE_URL = process.env.DATABASE_URL;
 
 const ENV = {
+  JWT: {
+    secret: JWT_SECRET_KEY,
+    expires: JWT_EXPIRES_IN,
+  },
 
-}
+  PORT: {
+    port: PORT,
+  },
+  NODEMAILER: {
+    password: NODEMAILER_PASS,
+    username: NODEMAILER_USERNAME,
+  },
+};
