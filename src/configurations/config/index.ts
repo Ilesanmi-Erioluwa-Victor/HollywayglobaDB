13af8aefacd656284bb6d76d3dba89a7edf8eb62
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-25 18:02:59 +0100

    Modifying my apps

--- src/configurations/config/index.ts
+++ src/configurations/config/index.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
-const JWT_SECRET_KEY = process.env.JWT_SERCRET_KEY;
+dotenv.config();
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 const PORT = process.env.PORT;
 const CRYPTO = process.env.CRYPTO;
 const NODEMAILER_PASS = process.env.NODEMAILER_PASS;
 const NODEMAILER_USERNAME = process.env.NODEMAILER_USERNAME;
-const DATABASE_URL = process.env.DATABASE_URL;
 
-dotenv.config();
 
 export const ENV = {
   JWT: {
