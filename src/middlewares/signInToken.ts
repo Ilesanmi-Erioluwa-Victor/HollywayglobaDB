f4fab5af16920dd53225f60d8be1a6f602ea36f9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-25 16:17:58 +0100

    Modifying my apps

--- src/middlewares/signInToken.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const signToken = (id:any) : string => {
-  return jwt.sign({ id }, `${process.env.JWT_SERCRET_KEY}`, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-};
-
-export default  signToken;
