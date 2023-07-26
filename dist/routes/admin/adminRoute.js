5a5268044958fa309bf650e960f2979644a9840d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-27 00:54:28 +0100

    Modifying my code in admin

--- dist/routes/admin/adminRoute.js
+++ dist/routes/admin/adminRoute.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const admin_controller_1 = require("../../model/admin/admin.controller");
 const authToken_1 = require("../../middlewares/auth/authToken");
 const adminRouter = express_1.default.Router();
-adminRouter.post("/admin_signup", admin_controller_1.adminSignUp);
+adminRouter.post('/admin_signup', admin_controller_1.adminSignUp);
 adminRouter.post('/admin_login', admin_controller_1.adminSignIn);
 adminRouter.put('/verify_account/:token', admin_controller_1.adminAccount_Verification);
 adminRouter.get('/:id/users/', authToken_1.AuthMiddleWare, authToken_1.adminRole, admin_controller_1.get_users);
--- src/routes/admin/adminRoute.ts
+++ src/routes/admin/adminRoute.ts
@@ -1,10 +1,15 @@
-import express from "express";
-import { adminSignUp, adminSignIn,get_users, adminAccount_Verification } from '../../model/admin/admin.controller';
-import { adminRole, AuthMiddleWare } from "../../middlewares/auth/authToken";
+import express from 'express';
+import {
+  adminSignUp,
+  adminSignIn,
+  get_users,
+  adminAccount_Verification,
+} from '../../model/admin/admin.controller';
+import { adminRole, AuthMiddleWare } from '../../middlewares/auth/authToken';
 
 const adminRouter = express.Router();
 
-adminRouter.post("/admin_signup", adminSignUp);
+adminRouter.post('/admin_signup', adminSignUp);
 adminRouter.post('/admin_login', adminSignIn);
 adminRouter.put('/verify_account/:token', adminAccount_Verification);
 
