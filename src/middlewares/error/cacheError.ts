fcf7ac145a84f9df4e9ce48de6335e1fb8635365
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-28 16:43:39 +0100

    Restructuring all logic

--- src/middlewares/error/cacheError.ts
+++ src/middlewares/error/cacheError.ts
@@ -1,6 +1,6 @@
 import { Error } from '../../interfaces/requestErrorInterface';
 
-export const throwError = (errorMsg: string, statusCode: number) => {
+export const throwError = (errorMsg: string, statusCode: number): Error => {
   const error: any = new Error(errorMsg);
   error.statusCode = statusCode;
   Error.captureStackTrace(error, throwError);
