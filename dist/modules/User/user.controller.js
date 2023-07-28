7915f463d8bd2e937fa12e8ff699fb54a4a775ba
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-28 17:40:25 +0100

    Restructuring all logic

--- dist/modules/User/user.controller.js
+++ dist/modules/User/user.controller.js
@@ -7,4 +7,5 @@ const express_1 = __importDefault(require("express"));
 const user_auth_service_1 = require("./services/user.auth.service");
 const route = express_1.default.Router();
 route.post('/signup', user_auth_service_1.createUser);
+route.post('/login', user_auth_service_1.loginUser);
 exports.default = route;
--- src/modules/User/user.controller.ts
+++ src/modules/User/user.controller.ts
@@ -3,5 +3,5 @@ import { createUser, loginUser } from './services/user.auth.service';
 
 const route = express.Router();
 route.post('/signup', createUser);
-
+route.post('/login', loginUser);
 export default route;
