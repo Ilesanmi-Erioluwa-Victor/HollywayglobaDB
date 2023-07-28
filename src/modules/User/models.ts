21097b6a04c600b80ac774150b1a0a3d43813b43
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-28 14:39:52 +0100

    Restructuring all logic

--- src/modules/User/models.ts
+++ src/modules/User/models.ts
@@ -1,9 +1,5 @@
 import { prisma } from '../../configurations/db';
 
-interface user {
-  id: string;
-  email: string;
-}
 export const findUser = async (user: user) => {
   const { id, email } = user;
   if (id) {
