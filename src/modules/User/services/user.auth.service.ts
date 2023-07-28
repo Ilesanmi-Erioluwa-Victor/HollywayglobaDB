509ba4d4be93be1861e708a868cd32ae018d64a0
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-28 14:11:21 +0100

    Restructuring all logic

--- src/modules/User/services/user.auth.service.ts
+++ src/modules/User/services/user.auth.service.ts
@@ -1,2 +1,5 @@
 import  bcrypt  from 'bcryptjs';
 import { RequestHandler, NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { throwError } from '../../../middlewares/error/cacheError';
+import { catchAsync } from '../../../helper/utils';
