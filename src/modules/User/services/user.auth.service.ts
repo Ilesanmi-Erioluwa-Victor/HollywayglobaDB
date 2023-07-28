bd00adfa74181831843cf72de2b4916934a6962e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-28 14:43:33 +0100

    Restructuring all logic

--- src/modules/User/services/user.auth.service.ts
+++ src/modules/User/services/user.auth.service.ts
@@ -1,13 +1,19 @@
-import  bcrypt  from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import { RequestHandler, NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { throwError } from '../../../middlewares/error/cacheError';
-import { catchAsync, ValidateMongoDbId, generateToken, createAccountVerificationToken, generatePasswordResetToken } from '../../../helper/utils';
+import {
+  catchAsync,
+  ValidateMongoDbId,
+  generateToken,
+  createAccountVerificationToken,
+  generatePasswordResetToken,
+} from '../../../helper/utils';
 
-export const createUser: RequestHandler= catchAsync( async(req: Request, res : Response, next : NextFunction) => {
-  try {
-    
-  } catch (error) {
-    
-  }  
-})
+export const createUser: RequestHandler = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { firstName, lastName, password, email, mobile } = req.body;
+    } catch (error) {}
+  }
+);
