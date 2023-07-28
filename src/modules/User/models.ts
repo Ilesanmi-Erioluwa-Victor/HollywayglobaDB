3e62843b4a39b68f13192b966a5cc437a5ab0825
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-28 14:22:21 +0100

    Restructuring all logic

---
+++ src/modules/User/models.ts
@@ -0,0 +1,2 @@
+import { prisma } from "../../configurations/db";
+
--- src/modules/User/services/user.auth.service.ts
+++ src/modules/User/services/user.auth.service.ts
@@ -3,3 +3,4 @@ import { RequestHandler, NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { throwError } from '../../../middlewares/error/cacheError';
 import { catchAsync, ValidateMongoDbId, generateToken, createAccountVerificationToken, generatePasswordResetToken } from '../../../helper/utils';
+
