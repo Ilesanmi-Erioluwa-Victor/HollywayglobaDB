9b388fdc681866e2b5a4ca8a12242766f7ef3aae
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-07-29 19:30:52 +0100

    first commit

--- dist/middlewares/_404.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pageNotFound = void 0;
-const pageNotFound = (req, res, next) => {
-    res.status(404).send('Page not found');
-};
-exports.pageNotFound = pageNotFound;
--- dist/utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsync = void 0;
-const catchAsync = (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next).catch((err) => next(err));
-    };
-};
-exports.catchAsync = catchAsync;
