60e333d438d5d6a064df2f49e4872dac4ff31fed
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-31 21:27:57 +0100

    Working on image resize function

--- dist/middlewares/image/resizeImage.js
+++ dist/middlewares/image/resizeImage.js
@@ -1,12 +1,22 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const multer_1 = __importDefault(require("multer"));
+const sharp_1 = __importDefault(require("sharp"));
+const path_1 = __importDefault(require("path"));
 const multerStorage = multer_1.default.memoryStorage();
-const MulterFilter = (req, file, cb) => {
-    // check file type
+const multerFilter = (req, file, cb) => {
     if (file.mimetype.startsWith('image')) {
         cb(null, true);
     }
@@ -16,3 +26,21 @@ const MulterFilter = (req, file, cb) => {
         });
     }
 };
+const profileImage = (0, multer_1.default)({
+    storage: multerStorage,
+    fileFilter: multerFilter,
+    limits: {
+        fileSize: 2000000,
+    },
+});
+const profilePhotoResize = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!req.file)
+        return next();
+    req.file.filename = `user-${Date.now()}-${req.file.originalname}`;
+    yield (0, sharp_1.default)(req.file.buffer)
+        .resize(250, 250)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(path_1.default.join(`public/images/profile/${req.file.filename}`));
+    next();
+});
--- src/middlewares/image/resizeImage.ts
+++ src/middlewares/image/resizeImage.ts
@@ -4,8 +4,8 @@ import path from 'path'
 
 const multerStorage = multer.memoryStorage();
 
-const MulterFilter = (req, file, cb) => {
-  // check file type
+const multerFilter = (req, file, cb) => {
+
   if (file.mimetype.startsWith('image')) {
     cb(null, true);
   } else {
@@ -13,4 +13,25 @@ const MulterFilter = (req, file, cb) => {
       message: 'Unsupported file type or format',
     });
   }
+};
+
+const profileImage = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+  limits: {
+    fileSize: 2000000,
+  },
+});
+
+const profilePhotoResize = async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${Date.now()}-${req.file.originalname}`;
+
+  await sharp(req.file.buffer)
+    .resize(250, 250)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(path.join(`public/images/profile/${req.file.filename}`));
+  next();
 };
