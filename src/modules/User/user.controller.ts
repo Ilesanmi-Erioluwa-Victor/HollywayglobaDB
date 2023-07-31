b205046751ee1f1b651d8f67acdfc61827e8b898
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-31 12:11:18 +0100

    working on cloudinary configuration

--- src/modules/User/user.controller.ts
+++ src/modules/User/user.controller.ts
@@ -22,6 +22,7 @@ route.post('/forgetPassword', forgetPasswordToken);
 route.put('/resetPassword/:token', resetPassword);
 route.get('/:id', AuthMiddleWare, isUserVerified, getUser);
 route.put('/updateProfile/:id', AuthMiddleWare, isUserVerified, updateUser);
+route.post('/uploadImage/:id', AuthMiddleWare, isUserVerified, updateUser);
 route.put('/password/:id', AuthMiddleWare, isUserVerified, updatePassword);
 route.put('/:id/verify_account/:token', accountVerification);
 export default route;
