891abf4997967e41605c3c17ea125653c596a45f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-31 12:04:02 +0100

    working on cloudinary configuration

--- src/configurations/cloudinary/index.ts
+++ src/configurations/cloudinary/index.ts
@@ -1,1 +1,8 @@
 import { v2 as cloudinary } from 'cloudinary'
+import { ENV } from '../config'
+
+cloudinary.config({
+    cloud_name: ENV.CLOUDIANRY.NAME,
+    api_key: ENV.CLOUDIANRY.KEY,
+    api_secret: ENV.CLOUDIANRY.SECRET
+})
