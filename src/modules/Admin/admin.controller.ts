2a7e0096128227908610dc95e574eaba1c5235c2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-03 18:05:08 +0100

    Working on admin section

--- src/modules/Admin/admin.controller.ts
+++ src/modules/Admin/admin.controller.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { AuthMiddleWare, adminRole } from '../../middlewares/auth/authToken';
-import { accountVerificationAdmin, adminSignup, loginAdmin } from './services/admin.auth.service';
+import {
+  accountVerificationAdmin,
+  adminSignup,
+  loginAdmin,
+  getUsersAdmin,
+} from './services/admin.auth.service';
 
 const route = express.Router();
 route.post('/admin_signup', adminSignup);
-route.post("/admin_login", loginAdmin)
-route.get("/admin/:id/users",)
+route.post('/admin_login', loginAdmin);
+route.get('/admin/:id/users', AuthMiddleWare, adminRole, getUsersAdmin);
 route.put('/:id/verify_account/:token', accountVerificationAdmin);
 
 export default route;
