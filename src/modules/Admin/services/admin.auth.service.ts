b74f6d30eb0874f983c49f3d63a1db4bb25f2732
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-03 15:11:11 +0100

    Working on admin section

--- src/modules/Admin/services/admin.auth.service.ts
+++ src/modules/Admin/services/admin.auth.service.ts
@@ -16,24 +16,27 @@ import { createAdminM, findAdminEmailM } from '../models';
 export const adminSignup: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { email, password, name } = req.body;
-        if (!email || !password || !name)
-          return next(
-            throwError(
-              'Missing credentials, please provide all required information',
-              StatusCodes.BAD_REQUEST
-            )
-            );
-        const existAdmin = await findAdminEmailM(email)
-        if (existAdmin)
-          return next(
-            throwError(
-              'You are already an admin, kindly login to your account',
-              StatusCodes.CONFLICT
-            )
-          );
-        const admin = await createAdminM(req.body)
-        
+      const { email, password, name } = req.body;
+      if (!email || !password || !name)
+        return next(
+          throwError(
+            'Missing credentials, please provide all required information',
+            StatusCodes.BAD_REQUEST
+          )
+        );
+      const existAdmin = await findAdminEmailM(email);
+      if (existAdmin)
+        return next(
+          throwError(
+            'You are already an admin, kindly login to your account',
+            StatusCodes.CONFLICT
+          )
+        );
+      const admin = await createAdminM(req.body);
+      res.status(StatusCodes.CREATED).json({
+        message: 'You have successfully created your account, log in now',
+        status: 'success',
+      });
     } catch (error: any) {
       if (!error.statusCode) {
         error.statusCode = 500;
