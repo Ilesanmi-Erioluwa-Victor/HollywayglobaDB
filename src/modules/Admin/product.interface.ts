c596e651bf810dd40bc2910f7d441cdbe3dc4103
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-05 11:16:30 +0100

    working on admin aspect

--- src/modules/Admin/product.interface.ts
+++ src/modules/Admin/product.interface.ts
@@ -1,13 +1,12 @@
-export interface createProductI  {
-    title: string;
-    slug: string;
-        description: string
-        price,
-        quantity,
-        images,
-        brand,
-        stock,
-        colors,
-        sold,
-        categoryId,
+export interface createProductI {
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  quantity: number;
+  images: string;
+  brand: string;
+  stock: number;
+  colors: string;
+  categoryId: string;
 }
