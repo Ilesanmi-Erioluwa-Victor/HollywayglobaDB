b98e464ecf834a5710d112c1d2f3fbde143c51c4
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-05 10:32:46 +0100

    Working on products

--- src/modules/Admin/services/admin.production.service.ts
+++ src/modules/Admin/services/admin.production.service.ts
@@ -1,3 +1,7 @@
+import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { throwError } from '../../../middlewares/error/cacheError';
 import { CustomRequest } from '../../../interfaces/custom';
+import { catchAsync } from '../../../helper/utils';
+
+export const createProduct: RequestHandler = catchAsync 
