b0d5f9eb232b8bd791e8204c6eed187046241bf8
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-05 11:09:03 +0100

    working on admin aspect

--- src/modules/Admin/product.models.ts
+++ src/modules/Admin/product.models.ts
@@ -1,6 +1,11 @@
 import { categoryI, signupAdmin } from './admin.interface';
 import { prisma } from '../../configurations/db';
 
-export const productId = async (id: string) => {
-    
+export const findProductIdM = async (id: string) => {
+    const product = await prisma.product.findUnique({
+        where : {
+            id
+        }
+    })
+    return product;
 }
