676e770affddbcc4b51b83450376cc5da291f9da
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-05 10:26:11 +0100

    Working on products

--- src/middlewares/error/cacheError.ts
+++ src/middlewares/error/cacheError.ts
@@ -1,7 +1,7 @@
 import { Error } from '../../interfaces/requestErrorInterface';
 
 export const throwError = (errorMsg: string, statusCode: number) => {
-  const error: any = new Error(errorMsg);
+  const error: Error = new Error(errorMsg);
   error.statusCode = statusCode;
   throw error;
 };
