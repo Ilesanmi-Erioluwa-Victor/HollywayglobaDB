ca3bd58ced35df535435eee26a7df73482462747
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-07 13:23:15 +0100

    having a bug in relation

--- src/modules/Product/product.controller.ts
+++ src/modules/Product/product.controller.ts
@@ -3,5 +3,5 @@ import { AuthMiddleWare, adminRole } from '../../middlewares/auth/authToken';
 import { createProduct } from "./services/product.service"
 
 const route = express.Router();
-route.post("/:id/admin/products", AuthMiddleWare, adminRole, createProduct)
+route.post("/admin/:id/product", AuthMiddleWare, adminRole, createProduct)
 export default route;
