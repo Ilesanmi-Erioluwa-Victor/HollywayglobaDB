5cbe01a8de7e168099d028917a637b7aa6f8a07c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-08 20:37:41 +0100

    working on products aspect

--- src/modules/Admin/product.controller.ts
+++ src/modules/Admin/product.controller.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { AuthMiddleWare, adminRole } from '../../middlewares/auth/authToken';
 import { createProduct } from './services/product.service';
+import { getProductsAdmin } from './services/admin.auth.service';
 
 const route = express.Router();
 route.post('/admin/:id/product', AuthMiddleWare, adminRole, createProduct);
+route.get('/admin/:id/products', AuthMiddleWare, adminRole, getProductsAdmin);
 export default route;
