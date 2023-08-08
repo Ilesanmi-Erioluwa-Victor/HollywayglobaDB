d5d5cb17f4e46e10f964d6c5bcca8787df7cf368
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-08 20:22:30 +0100

    Everything working

--- src/modules/Product/product.models.ts
+++ src/modules/Product/product.models.ts
@@ -43,34 +43,7 @@ export const createProductM = async (productI: createProductI) => {
   return product;
 };
 
-export const getProductM = async (productI: createProductI) => {
-  const {
-    title,
-    slug,
-    description,
-    price,
-    quantity,
-    // images,
-    stock,
-    colors,
-    brand,
-    categoryId,
-    adminId
-  } = productI;
-  const product = await prisma.product.create({
-    data: {
-      title,
-      slug,
-      description,
-      price,
-      quantity,
-      brand,
-      // images,
-      stock,
-      colors,
-      categoryId,
-      adminId
-    },
-  });
+export const getProductsM = async () => {
+  const product = await prisma.product.findMany()
   return product;
 };
