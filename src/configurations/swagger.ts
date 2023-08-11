c76d0de2575f42847102b1f13506792d7f7a0d07
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-11 02:50:37 +0100

    Working on swagger ui for api doc

--- src/configurations/swagger.ts
+++ src/configurations/swagger.ts
@@ -2,6 +2,8 @@ import swaggerAutogen from "swagger-autogen"
 
 const doc = {
     info: {
-        title :"Hollywaygloba API Documentation"
-    }
+        title: "Hollywaygloba API Documentation",
+        description : ""
+    },
+    host : "localhost:8080/api/v1"
 }
