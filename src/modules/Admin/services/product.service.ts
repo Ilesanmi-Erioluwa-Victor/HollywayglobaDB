20401e39f4c50c1100f9489ecd85035b034be99b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-12 22:27:30 +0100

    first commmit

--- src/modules/Admin/services/product.service.ts
+++ src/modules/Admin/services/product.service.ts
@@ -231,7 +231,7 @@ export const editProductImagesAdmin: RequestHandler = catchAsync(
 export const TopTenProducts: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      console.log(req.query)
+      console.log()
     } catch (error: any) {
       if (!error.statusCode) {
         error.statusCode = 500;
