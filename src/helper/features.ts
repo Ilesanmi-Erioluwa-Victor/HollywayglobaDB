2fee2746c951bc4907d7563758288d3667f628d8
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-14 16:04:52 +0100

    Working on sorting, pagination, filtering of products

--- src/helper/features.ts
+++ src/helper/features.ts
@@ -1,52 +1,82 @@
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
+import { Prisma } from '@prisma/client';
 
-  filter() {
+type QueryString = {
+  page?: string;
+  sort?: string;
+  limit?: string;
+  fields?: string;
+  // Add more query parameters if needed
+};
+
+class APIFeatures {
+  constructor(
+    private query: Prisma.ProductFindManyArgs,
+    private queryString: QueryString
+  ) {}
+
+  filter(): this {
     const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
+    excludedFields.forEach((el: any) => delete queryObj[el]);
 
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // Adapt the query to your Prisma model's query structure
+    const prismaQuery: Prisma.ProductWhereInput = {
+      // Construct your Prisma query based on queryObj
+    };
 
-    this.query = this.query.find(JSON.parse(queryStr));
+    this.query.where = prismaQuery;
 
     return this;
   }
 
-  sort() {
+  sort(): this {
     if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
+      // Adapt and set the orderBy option of the Prisma query
+      this.query.orderBy = {
+        // Construct your Prisma orderBy object based on this.queryString.sort
+      };
     } else {
-      this.query = this.query.sort('-createdAt');
+      // Default sorting
+      this.query.orderBy = {
+        createdAt: 'asc',
+      };
     }
 
     return this;
   }
 
-  limitFields() {
+  limitFields(): this {
     if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select('-__v');
+      // Adapt the select option of the Prisma query
+      this.query.select = {
+        // Construct your Prisma select fields based on this.queryString.fields
+      };
     }
 
     return this;
   }
 
-  paginate() {
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
+  paginate(): this {
+    const page = this.queryString.page ? parseInt(this.queryString.page) : 1;
+    const limit = this.queryString.limit
+      ? parseInt(this.queryString.limit)
+      : 100;
     const skip = (page - 1) * limit;
 
-    this.query = this.query.skip(skip).limit(limit);
+    // Adapt the skip and take options of the Prisma query
+    this.query.skip = skip;
+    this.query.take = limit;
 
     return this;
   }
 }
+
+// Usage example:
+// const prisma = new PrismaClient(); // Create your Prisma client instance
+// const query: Prisma.ProductFindManyArgs = {}; // Initialize your Prisma query
+// const queryString: QueryString = req.query; // Assuming you get this from Express request
+// const features = new APIFeatures(query, queryString);
+// features.filter().sort().limitFields().paginate();
+// const result = await prisma.product.findMany(features.query);
+
+export default APIFeatures;
