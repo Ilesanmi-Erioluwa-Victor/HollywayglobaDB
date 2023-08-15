241dfe953839a3654bbc2e24cb6c9f92f1c7568d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-15 14:35:33 +0100

    Working on cart

--- src/modules/User/models.ts
+++ src/modules/User/models.ts
@@ -213,35 +213,3 @@ export const findUserWithAddressM = async (id: string) => {
   return user;
 };
 
-export const userWishListM = async (
-  userId: string,
-  productId: string,
-  quantity: number
-) => {
-  const wishList = await prisma.productWishList.create({
-    data: {
-      user: { connect: { id: userId } },
-      product: { connect: { id: productId } },
-      quantity: quantity,
-    },
-    select: {
-      id: true,
-      quantity: true,
-      createdAt: false,
-      updatedAt: false,
-      product: {
-        select: {
-          title: true,
-          price: true,
-          colors: true,
-          description: true,
-          brand: true,
-          slug: true,
-          images: true,
-        },
-      },
-    },
-  });
-
-  return wishList;
-};
---
+++ src/modules/User/models/user.cart.model.ts
@@ -0,0 +1,32 @@
+export const userWishListM = async (
+  userId: string,
+  productId: string,
+  quantity: number
+) => {
+  const wishList = await prisma.productWishList.create({
+    data: {
+      user: { connect: { id: userId } },
+      product: { connect: { id: productId } },
+      quantity: quantity,
+    },
+    select: {
+      id: true,
+      quantity: true,
+      createdAt: false,
+      updatedAt: false,
+      product: {
+        select: {
+          title: true,
+          price: true,
+          colors: true,
+          description: true,
+          brand: true,
+          slug: true,
+          images: true,
+        },
+      },
+    },
+  });
+
+  return wishList;
+};
