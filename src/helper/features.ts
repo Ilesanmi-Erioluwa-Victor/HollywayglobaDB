4b0621f8a2067ae2a071fc04a4ba67585be39ce5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-15 08:49:18 +0100

    Working on project

--- src/helper/features.ts
+++ src/helper/features.ts
@@ -1,99 +1,97 @@
-import { Model, Prisma } from '@prisma/client';
-import { prisma } from '../configurations/db';
+// import { Model, Prisma } from '@prisma/client';
+// import { prisma } from '../configurations/db';
 
-type QueryParams = {
-  [key: string]: string;
-};
+// type QueryParams = {
+//   [key: string]: string;
+// };
 
-class APIFeatures<T> {
-  query: Model<T>;
-  queryString: QueryParams;
+// class APIFeatures<T> {
+//   query: Model<T>;
+//   queryString: QueryParams;
 
-  constructor(query: Model<T>, queryString: QueryParams) {
-    this.query = query;
-    this.queryString = queryString;
-  }
+//   constructor(query: Model<T>, queryString: QueryParams) {
+//     this.query = query;
+//     this.queryString = queryString;
+//   }
 
-  filter(): this {
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
+//   filter(): this {
+//     const queryObj = { ...this.queryString };
+//     const excludedFields = ['page', 'sort', 'limit', 'fields'];
+//     excludedFields.forEach((el) => delete queryObj[el]);
 
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+//     let queryStr = JSON.stringify(queryObj);
+//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    this.query = prisma.product.findMany({
-      where: JSON.parse(queryStr),
-    });
+//     this.query = prisma.product.findMany({
+//       where: JSON.parse(queryStr),
+//     });
 
-    return this;
-  }
+//     return this;
+//   }
 
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = prisma.product.findMany({
-        orderBy: [
-          {
-            title: 'asc',
-          },
-          {
-            category: sortBy,
-          },
-        ],
-      });
-    } else {
-      this.query = prisma.product.findMany({
-        orderBy: {
-          createdAt: 'desc',
-        },
-      });
-    }
+//   sort() {
+//     if (this.queryString.sort) {
+//       const sortBy = this.queryString.sort.split(',').join(' ');
+//       this.query = prisma.product.findMany({
+//         orderBy: [
+//           {
+//             title: 'asc',
+//           },
+//           {
+//             category: sortBy,
+//           },
+//         ],
+//       });
+//     } else {
+//       this.query = prisma.product.findMany({
+//         orderBy: {
+//           createdAt: 'desc',
+//         },
+//       });
+//     }
 
-    return this;
-  }
+//     return this;
+//   }
 
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = prisma.product.findMany({
-        select: {
-          yourFieldName: true,
-        },
-      });
-    } else {
-      this.query = prisma.product.findMany({
-        select: {
-          __v: false,
-        },
-      });
-    }
+//   limitFields() {
+//     if (this.queryString.fields) {
+//       const fields = this.queryString.fields.split(',').join(' ');
+//       this.query = prisma.product.findMany({
+//         select: {
+//           yourFieldName: true,
+//         },
+//       });
+//     } else {
+//       this.query = prisma.product.findMany({
+//         select: {
+//           __v: false,
+//         },
+//       });
+//     }
 
-    return this;
-  }
+//     return this;
+//   }
 
-  paginate(): this {
-       const page = parseInt(this.queryString.page || '1', 10);
-       const limit = parseInt(this.queryString.limit || '100', 10);
-       const skip = (page - 1) * limit;
+//   paginate(): this {
+//        const page = parseInt(this.queryString.page || '1', 10);
+//        const limit = parseInt(this.queryString.limit || '100', 10);
+//        const skip = (page - 1) * limit;
 
-    this.query = prisma.product.findMany({
-      skip,
-      take: limit,
-    });
+//     this.query = prisma.product.findMany({
+//       skip,
+//       take: limit,
+//     });
 
-    return this;
-  }
-}
+//     return this;
+//   }
+// }
 
+// // Usage example:
+// // const prisma = new PrismaClient(); // Create your Prisma client instance
+// // const query: Prisma.ProductFindManyArgs = {}; // Initialize your Prisma query
+// // const queryString: QueryString = req.query; // Assuming you get this from Express request
+// // const features = new APIFeatures(query, queryString);
+// // features.filter().sort().limitFields().paginate();
+// // const result = await prisma.product.findMany(features.query);
 
-
-// Usage example:
-// const prisma = new PrismaClient(); // Create your Prisma client instance
-// const query: Prisma.ProductFindManyArgs = {}; // Initialize your Prisma query
-// const queryString: QueryString = req.query; // Assuming you get this from Express request
-// const features = new APIFeatures(query, queryString);
-// features.filter().sort().limitFields().paginate();
-// const result = await prisma.product.findMany(features.query);
-
-export default APIFeatures;
+// export default APIFeatures;
