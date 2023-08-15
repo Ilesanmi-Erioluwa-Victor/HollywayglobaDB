8f05529e826af124cc005ccd99f96823fe557002
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-15 14:19:50 +0100

    Refactoring codebase

--- src/modules/User/services/user.auth.service.ts
+++ src/modules/User/services/user.auth.service.ts
@@ -435,33 +435,3 @@ export const editAddress: RequestHandler = catchAsync(
   }
 );
 
-export const addToWishlist: RequestHandler = async (
-  req: CustomRequest,
-  res: Response,
-  next: NextFunction
-) => {
-  const userId = req.authId;
-  ValidateMongoDbId(userId as string);
-
-  const { productId, quantity } = req.body;
-
-  try {
-    if (!userId || !productId || !quantity)
-      next(
-        new AppError('Missing required information', StatusCodes.BAD_REQUEST)
-      );
-
-    const userWishlistItem = await userWishListM(
-      userId as string,
-      productId,
-      quantity
-    );
-
-    res.json({ message: 'Product added to wishlist', data: userWishlistItem });
-  } catch (error: any) {
-    if (!error.statusCode) {
-      error.statusCode = 500;
-    }
-    next(error);
-  }
-};
