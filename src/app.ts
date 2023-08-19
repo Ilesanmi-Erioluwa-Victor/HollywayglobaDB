9e4097c892cf82436fcb4af0f47a3b395c588a6d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-19 07:36:06 +0100

    Refactoring codebase

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,22 @@ import ErrorHandlerMiddleware from './middlewares/error';
 import { SanitizeInputMiddleware } from './middlewares/sanitize';
 import { customTime } from './interfaces/custom';
 import { _404 } from './middlewares/error/_404Page';
+
+const app: Application = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(helmet());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.set('Methods', 'GET, POST, PUT, DELETE , PATCH');
+  res.set('Access-Control-Allow-Credentials', 'true');
+  res.set('content-type', 'application/json');
+  next();
+});
--- src/server.ts
+++ src/server.ts
@@ -4,24 +4,7 @@
 import { ENV } from './configurations/config';
 
 
-const app: Application = express();
 
-app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.json({ limit: '10kb' }));
-
-app.use(helmet());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.set('Methods', 'GET, POST, PUT, DELETE , PATCH');
-  res.set('Access-Control-Allow-Credentials', 'true');
-  res.set('content-type', 'application/json');
-  next();
-});
 
 ENV.MODE.DEVELOPMENT === 'development' ? app.use(morgan('dev')) : '';
 
