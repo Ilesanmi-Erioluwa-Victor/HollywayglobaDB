4a40d800ea0e32dbd5b7bf95b32febbc8c8228a1
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-19 07:34:35 +0100

    Refactoring codebase

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,9 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import morgan from 'morgan';
+
+import adminRoute from './modules/Admin/routes/admin.routes';
+import userRoute from './modules/User/routes/user.routes';
+import productRoute from './modules/Admin/routes/admin.routes';
+import ErrorHandlerMiddleware from './middlewares/error';
+import { SanitizeInputMiddleware } from './middlewares/sanitize';
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
 
 
-import adminRoute from './modules/Admin/routes/admin.routes';
-import userRoute from './modules/User/routes/user.routes';
-import productRoute from './modules/Admin/routes/admin.routes';
-import ErrorHandlerMiddleware from './middlewares/error';
-import { SanitizeInputMiddleware } from './middlewares/sanitize';
+
 import { ENV } from './configurations/config';
 import { customTime } from './interfaces/custom';
 import { _404 } from './middlewares/error/_404Page';
