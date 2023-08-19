290a7129529750f582f1151c9f2d79bdc5c90605
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-19 07:45:51 +0100

    Refactoring codebase

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,17 @@
 import { ENV } from './configurations/config';
-import app from "./app"
+import app from './app';
 class Server {
   static async Connection() {
     try {
-      
-    } catch (error) {
-      
+      app.listen(ENV.PORT.PORT || 3000, () => {
+        console.log(`App running on port ${ENV.PORT.PORT || 5000}`);
+      });
+    } catch (error: any) {
+      console.log(error.message);
     }
   }
 }
-const startConnection = async () => {
-  try {
-    app.listen(ENV.PORT.PORT || 5000, () => {
-      console.log(`App running on port ${ENV.PORT.PORT || 5000}`);
-    });
-  } catch (error: any) {
-    console.log(error.message);
-  }
-};
-startConnection();
+
+
 
 export default new Server();
