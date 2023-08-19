e624f94ec62e4c7323d183f9aa9f6e370af039ce
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-19 15:00:47 +0100

    refcatoring code

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -22,10 +22,15 @@ class Utils {
       new AppError('Invalid Id passed, check your Id', StatusCodes.BAD_REQUEST);
   }
 
-  static async accountVerificationTokenUser(userId: string) {
+  static async accountVerificationTokenUser(
+    userId: string,
+    account: string
+  ) {
     const verificationToken = crypto.randomBytes(32).toString('hex');
     const tokenExpiration = new Date(Date.now() + 30 * 60 * 1000);
 
+    let accountType = account;
+    
     const user = await prisma.user.update({
       where: { id: userId },
       data: {
