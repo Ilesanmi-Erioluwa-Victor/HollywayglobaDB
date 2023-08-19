409b5aebbeeaeb2a5d3d4b14163c03500c2dbd5c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-19 07:33:55 +0100

    Refactoring codebase

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,4 @@
-import express, { Application, NextFunction, Request, Response } from "express";
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import helmet from 'helmet';
-import morgan from 'morgan';
+
 
 import adminRoute from './modules/Admin/routes/admin.routes';
 import userRoute from './modules/User/routes/user.routes';
