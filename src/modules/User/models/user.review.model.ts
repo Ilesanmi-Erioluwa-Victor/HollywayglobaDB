a16b3f5d3f82cf880ac6ed5b616927f5eba6c1c3
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-11 17:50:09 +0100

    working on reviews

---
+++ src/modules/User/models/user.review.model.ts
@@ -0,0 +1,1 @@
+import { prisma } from '../../../configurations/db';
---
+++ src/modules/User/services/user.review.service.ts
@@ -0,0 +1,10 @@
+import { RequestHandler, NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { throwError } from '../../../middlewares/error';
+
+import { Utils } from '../../../helper/utils';
+
+import { CustomRequest } from '../../../interfaces/custom';
+
+const { catchAsync, ValidateMongoDbId } = Utils;
