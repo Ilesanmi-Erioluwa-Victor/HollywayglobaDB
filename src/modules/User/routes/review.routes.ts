1a3a7df51b682e93104ad3a6f0d50c7530acddaa
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-12 22:16:16 +0100

    working on get reviews

--- src/modules/User/routes/review.routes.ts
+++ src/modules/User/routes/review.routes.ts
@@ -13,14 +13,14 @@ import {
 const route = express.Router();
 
 route.post(
-  '/user/:id/product/:productId/review',
+  '/:id/product/:productId/review',
   Token,
   VerifiedUser,
   createReview
 );
 
 route.get(
-  '/user/:id/product/:productId/review/:reviewId',
+  '/:id/product/:productId/review/:reviewId',
   Token,
   VerifiedUser,
   getReview
@@ -34,7 +34,7 @@ route.put(
 );
 
 route.put(
-  '/user/:id/product/:productId/review/:reviewId',
+  '/:id/product/:productId/review/:reviewId',
   Token,
   VerifiedUser,
   editReview
