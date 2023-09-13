d6907c2a6d608d63479d7b51fbf0b7bfae963a6b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-13 15:27:12 +0100

    working on cart and order route

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,11 @@ import userRoute from './modules/User/routes/user.routes';
 
 import productRoute from './modules/Admin/routes/admin.routes';
 
-import reviewRoute from "./modules/User/routes/review.routes"
+import reviewRoute from './modules/User/routes/review.routes';
+
+import orderRoute from './modules/User/routes/review.routes';
+
+import cartRoute from './modules/User/routes/review.routes';
 
 import { requestErrorTypings } from './types';
 import { SanitizeInputMiddleware } from './middlewares/sanitize';
