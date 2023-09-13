1cae42eebbf29aafbb4e2b166440508d903be255
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-13 23:15:25 +0100

    working on Validtors

--- src/middlewares/validators/Validator.ts
+++ src/middlewares/validators/Validator.ts
@@ -2,11 +2,15 @@ import { Request, Response, NextFunction } from 'express';
 import { body, validationResult } from 'express-validator';
 
 export class customValidator {
-    static createUserValidation() {
-        return [
-          body('username').notEmpty().isString(),
-          body('email').isEmail(),
-          body('password').isLength({ min: 6 }),
-        ];
+  static createUserValidation() {
+    return [
+      body('firstName')
+        .notEmpty()
+        .isString()
+        .withMessage('firstName is required'),
+      body('lastName').isEmail().withMessage('lastName is required'),
+      body('password').notEmpty().withMessage('Password is required'),
+      body('mobile').notEmpty().withMessage('Phone number is required'),
+    ];
   }
 }
