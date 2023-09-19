cedb032ad1b234c5a5b3176a89c4cfd921da77fe
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-19 07:34:51 +0100

    Couldn't fix prisma error

--- src/middlewares/auth/index.ts
+++ src/middlewares/auth/index.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 
 import { StatusCodes } from 'http-status-codes';
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,2 @@
-class AppError extends Error {
-  statusCode: number;
-  status: string;
-  // isOperational: boolean;
+import jwt from 'jsonwebtoken';
 
-  constructor(message: string, statusCode: number) {
-    super(message);
-    this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-  }
-}
-
-export default AppError;
