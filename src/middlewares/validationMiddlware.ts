ebe2227ceabae2a08f6f8b943e9d0aa0a4c8e7d0
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-19 04:18:29 +0100

    working on errors

--- src/middlewares/validationMiddlware.ts
+++ src/middlewares/validationMiddlware.ts
@@ -1,7 +1,33 @@
 import { body, param, validationResult } from 'express-validator';
-
+import { Request, Response, NextFunction } from "express"
 import {
   BadRequestError,
   NotFoundError,
   UnauthorizedError,
 } from '../errors/customError';
+
+const withValidationErrors = (validateValues : any) => {
+  return [
+    validateValues,
+    (req: Request, res : Response, next : NextFunction) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const errorMessages = errors
+          .array()
+          .map((error : any) => `${error.path} : ${error.msg}`);
+
+        // const firstMessage = errorMessages[0];
+        // console.log(Object.getPrototypeOf(firstMessage));
+        if (errorMessages[0].startsWith('no job')) {
+          throw new NotFoundError(errorMessages);
+        }
+        if (errorMessages[0].startsWith('not authorized')) {
+          console.log('from Unauthorized', errorMessages);
+          throw new UnauthorizedError('not authorized to access this route');
+        }
+        throw new BadRequestError(errorMessages);
+      }
+      next();
+    },
+  ];
+};
