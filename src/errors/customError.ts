d5110c3db2f50f1727fdeae54f33411efe945ef9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-19 03:12:08 +0100

    working on errors

--- src/errors/customError.ts
+++ src/errors/customError.ts
@@ -9,3 +9,29 @@ export class NotFoundError extends Error {
     this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
+
+export class BadRequestError extends Error {
+  statusCode: number;
+  constructor(message: string) {
+    super(message);
+    this.name = 'BadRequestError';
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+export class UnauthenticatedError extends Error {
+  statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.name = 'UnauthenticatedError';
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+export class UnauthorizedError extends Error {
+  statusCode: number;
+  constructor(message: string) {
+    super(message);
+    this.name = 'UnauthorizedError';
+    this.statusCode = StatusCodes.FORBIDDEN;
+  }
+}
