ac58ef275963feb8f8d26fa2a787ccee1c446a16
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-19 07:36:30 +0100

    working on createJWT

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,9 @@
 import jwt from 'jsonwebtoken';
 
+export const createJwt = (payload) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  return token;
+};
