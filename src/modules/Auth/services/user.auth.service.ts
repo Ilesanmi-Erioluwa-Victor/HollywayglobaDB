65c0742d62b1a6d63fa6caa0f4881ccf2e75aaae
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-19 22:52:57 +0100

    fixed email error

--- src/modules/Auth/services/user.auth.service.ts
+++ src/modules/Auth/services/user.auth.service.ts
@@ -84,29 +84,21 @@ export const forgetPasswordToken: RequestHandler = catchAsync(
 
     if (!user) throw new NotFoundError('no user found');
 
-    try {
-      const resetToken = generatePasswordResetToken();
+    const resetToken = generatePasswordResetToken();
 
-      const expirationTime = new Date();
-      expirationTime.setHours(expirationTime.getHours() + 1);
+    const expirationTime = new Date();
+    expirationTime.setHours(expirationTime.getHours() + 1);
 
-      const passwordReset = await forgetPasswordTokenM(
-        await resetToken,
-        expirationTime,
-        user.id as string
-      );
-      console.log('<<<<resetTone', resetToken, '>>>>>>>>', passwordReset);
+    const passwordReset = await forgetPasswordTokenM(
+      await resetToken,
+      expirationTime,
+      user.id as string
+    );
 
-      await sendMailToken('user', passwordReset, req, res, next);
-      res.json({
-        message: `A reset token has been sent to your gmail`,
-        status: 'success',
-      });
-    } catch (error: any) {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
-    }
+    await sendMailToken('user', passwordReset, req, res, next);
+    res.json({
+      message: `A reset token has been sent to your gmail`,
+      status: 'success',
+    });
   }
 );
