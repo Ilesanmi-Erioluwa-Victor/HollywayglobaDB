ee4a7d22f98d7f53628ded8020745c9b2e99f525
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-19 03:09:59 +0100

    working on errors

--- src/errors/customError.ts
+++ src/errors/customError.ts
@@ -1,1 +1,11 @@
 import { StatusCodes } from 'http-status-codes';
+
+export class NotFoundError extends Error {
+  statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.name = 'NotFoundError';
+    this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
