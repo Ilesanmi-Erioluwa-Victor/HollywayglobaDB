1f372f3a689105c438625d0538fa8a047c49cc93
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-19 05:08:19 +0100

    working on errors

--- src/modules/User/models/user.auth.model.ts
+++ src/modules/User/models/user.auth.model.ts
@@ -2,27 +2,9 @@ import { prisma } from '../../../configurations/db';
 
 import { Utils } from '../../../helper/utils';
 
-import { signupUser } from '../user.interface';
-
 const { generateToken, hashedPassword, accountVerificationToken } = Utils;
 
 export class userQueries {
-  static async createUserM(user: signupUser) {
-    const { firstName, lastName, email, mobile, password } = user;
-    const createUser = await prisma.user.create({
-      data: {
-        firstName,
-        lastName,
-        email: email.toLocaleLowerCase(),
-        mobile,
-        password: await hashedPassword(password),
-      },
-    });
-
-    generateToken(createUser?.id as string);
-    const tokenUser = await accountVerificationToken('user', createUser?.id);
-    return tokenUser;
-  }
 
   static async findUserMId(id: string) {
     const userId = await prisma.user.findUnique({
