fa15102a02bb101d8e5b25113c7384877535cb69
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-21 13:07:36 +0100

    working on admin Auth

--- src/modules/Admin/services/product.service.ts
+++ src/modules/Admin/services/product.service.ts
@@ -16,6 +16,7 @@ import {
 
 import {
   BadRequestError,
+  Forbidden,
   NotFoundError,
   UnauthenticatedError,
   UnauthorizedError,
@@ -51,116 +52,50 @@ export const getProductsAdmin: RequestHandler = catchAsync(
     res: { paginatedResult: any } & Response,
     next: NextFunction
   ) => {
-    const { id } = req?.params;
-
-    ValidateMongoDbId(id);
-
-    if (!id)
-      return throwError('No Admin record found', StatusCodes.BAD_REQUEST);
-    try {
-      await getPaginatedProducts(req, res, next);
-    } catch (error: any) {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
-    }
+    await getPaginatedProducts(req, res, next);
   }
 );
 
 export const getProductAdmin: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { id, productId } = req?.params;
+    const product = await findProductIdM(req.params.productId);
 
-    ValidateMongoDbId(id);
-    ValidateMongoDbId(productId);
+    if (!product) throw new NotFoundError('no product found ...');
 
-    if (!id)
-      return throwError('No Admin record found', StatusCodes.BAD_REQUEST);
-    if (!productId)
-      return throwError('No product record found', StatusCodes.BAD_REQUEST);
-
-    try {
-      const product = await findProductIdM(productId);
-
-      if (!product)
-        throwError('No product record found', StatusCodes.BAD_REQUEST);
-
-      res.json({
-        status: 'Success',
-        data: product,
-      });
-    } catch (error: any) {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
-    }
+    res.json({
+      status: 'Success',
+      data: product,
+    });
   }
 );
 
 export const deleteProductAdmin: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { id, productId } = req?.params;
+    const product = await deleteProductM(req.params.productId);
 
-    ValidateMongoDbId(id);
-    ValidateMongoDbId(productId);
-
-    if (!id) throwError('No Admin record found', StatusCodes.BAD_REQUEST);
-
-    if (!productId)
-      throwError('No product record found', StatusCodes.BAD_REQUEST);
-
-    try {
-      const product = await deleteProductM(productId);
-      res.json({
-        status: 'Success',
-        message: 'You have successfully deleted this product',
-      });
-    } catch (error: any) {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
-    }
+    if (!product) throw new BadRequestError('something went wrong ...');
+    res.json({
+      status: 'success',
+      message: 'you have successfully deleted this product',
+    });
   }
 );
 
 export const editProductAdmin: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { id, productId } = req?.params;
-
-    ValidateMongoDbId(id);
-    ValidateMongoDbId(productId);
-
-    if (!id)
-      return throwError('No Admin record found', StatusCodes.BAD_REQUEST);
-
-    if (!productId)
-      return throwError('No product record found', StatusCodes.BAD_REQUEST);
-
-    if (!req.files)
-      return throwError(
-        'Sorry, please select an image to be uploaded',
-        StatusCodes.BAD_REQUEST
+    if (req.files)
+      throw new Forbidden(
+        "sorry, you can't edit product image with this endpoint"
       );
 
-    try {
-      const product = await editProductM(productId, req.body);
+    const product = await editProductM(req.params.productId, req.body);
 
-      if (!product)
-        return throwError('No product record found', StatusCodes.BAD_REQUEST);
+    if (!product) throw new NotFoundError('no product found');
 
-      res.json({
-        status: 'Success',
-        message: 'You have successfully updated this product',
-      });
-    } catch (error: any) {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
-    }
+    res.json({
+      status: 'success',
+      message: 'you have successfully updated this product',
+    });
   }
 );
 
