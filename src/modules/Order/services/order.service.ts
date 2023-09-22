6d901eab5152bda39436331b1607dc81129cbb86
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-23 00:42:54 +0100

    working on order

--- src/modules/Order/services/order.service.ts
+++ src/modules/Order/services/order.service.ts
@@ -1,3 +1,9 @@
-export const getOrder = () => {
-    
-}
+import { NextFunction, Response, Request } from 'express';
+
+import { Utils } from '../../../helper/utils';
+
+import { NotFoundError } from '../../../errors/customError';
+
+const { catchAsync } = Utils;
+
+export const getOrder = () => {};
