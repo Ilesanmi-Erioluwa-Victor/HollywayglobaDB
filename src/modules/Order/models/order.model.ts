28aa072f8bec3dde61f6b81f501f2981bc788903
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-25 17:14:37 +0100

    refactoring order service

--- src/modules/Order/models/order.model.ts
+++ src/modules/Order/models/order.model.ts
@@ -1,5 +1,21 @@
 import { prisma } from '../../../configurations/db';
 
 export class orderQuery {
-    
+  static async existCart(cartId: string, userId: string) {
+    const userCart = await prisma.cart.findUnique({
+      where: {
+        id: cartId,
+        userId,
+      },
+      include: {
+        items: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    });
+
+    return userCart;
+  }
 }
